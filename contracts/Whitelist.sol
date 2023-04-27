eb6c129943a0cd2c856a302b89027ab40172d5a4
Hassan Habib Tahir <47211929+HassanHabibTahir@users.noreply.github.com>
2023-04-27 09:36:06 +0500

    added address to whitelist

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Whitelist {
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    //6
+    mapping(address => bool) public whitelistedAddresses;
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    // NOTE: Don't change this variable name, as it will be part of verification;
+    // 5
+    uint8 public numAddressesWhitelisted;
+
+    // Setting the Max number of whitelisted addresses
+    // User will put the value at the time of deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /**
+     addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+     */
+    function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
+        //w-t
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses cant be added, limit reached"
+        );
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
+    }
+
+    function addAddressToWhitelist(address[] memory addresses) public {
+        // Add maxWhitelistedAddresses addresses to the whitelist
+        for (uint i = 0; i < maxWhitelistedAddresses; i++) {
+            whitelistedAddresses[addresses[i]] = true;
+        }
+    }
+
+    function removeAddressFromWhitelist(address oldAddress) public {
+        whitelistedAddresses[oldAddress] = false;
+    }
+}
